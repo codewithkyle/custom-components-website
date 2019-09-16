b760f1aa1117ce6a2f26d163aaa3cac301fe258f
Kyle Andrews <codingwithkyle@gmail.com>
2019-09-16 11:44:38 +0000

    Updates

---
+++ assets/1568634277468/outline-primary.js
@@ -0,0 +1,6 @@
+class OutlinePrimary extends HTMLElement {
+    connectedCallback() {
+        console.log('outline-primary component has been connected.');
+    }
+}
+customElements.define('outline-primary', OutlinePrimary);
---
+++ assets/1568634277468/solid-danger.js
@@ -0,0 +1,6 @@
+class SolidDanger extends HTMLElement {
+    connectedCallback() {
+        console.log('solid-danger component has been connected.');
+    }
+}
+customElements.define('solid-danger', SolidDanger);
