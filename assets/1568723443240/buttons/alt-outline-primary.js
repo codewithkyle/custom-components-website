d9ef0a8ca81d3a4a074d8972eea60c05ae3e851f
Kyle Andrews <codingwithkyle@gmail.com>
2019-09-17 12:30:44 +0000

    Updates

---
+++ assets/1568723443240/buttons/alt-outline-primary.js
@@ -0,0 +1,6 @@
+class AltOutlinePrimary extends HTMLElement {
+    connectedCallback() {
+        console.log('alt-outline-primary component has been connected.');
+    }
+}
+customElements.define('alt-outline-primary', AltOutlinePrimary);
---
+++ assets/1568723443240/buttons/raised-solid-primary.js
@@ -0,0 +1,6 @@
+class RaisedSolidPrimary extends HTMLElement {
+    connectedCallback() {
+        console.log('raised-solid-primary component has been connected.');
+    }
+}
+customElements.define('raised-solid-primary', RaisedSolidPrimary);
