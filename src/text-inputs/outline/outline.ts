1208dcf735ed9e5263922cdbefb3a3572d24530e
Kyle Andrews <kylea@page.works>
2019-09-19 08:23:54 -0400

    Adds outline input component

--- src/text-inputs/outline/outline.ts
+++ src/text-inputs/outline/outline.ts
@@ -1,8 +1,40 @@
-class OutlineComponent extends HTMLElement
+class OutlineInputComponent extends HTMLElement
 {
+	private _input : HTMLInputElement;
+
+	constructor()
+	{
+		super();
+		this._input = this.querySelector('input');
+	}
+
+	private handleBlurEvent:EventListener = this.validate.bind(this);
+
+	private validate() : void
+	{
+		if (this._input.value === '')
+		{
+			this._input.classList.remove('has-value');
+		}
+		else
+		{
+			this._input.classList.add('has-value');
+		}
+
+		if (!this._input.validity.valid)
+		{
+			this._input.classList.add('is-invalid');
+			this._input.reportValidity();
+		}
+		else
+		{
+			this._input.classList.remove('is-invalid');
+		}
+	}
+
 	connectedCallback()
 	{
-		console.log('outline component has been connected.');
+		this._input.addEventListener('blur', this.handleBlurEvent);
 	}
 }
-customElements.define('outline-component', OutlineComponent);
+customElements.define('outline-input-component', OutlineInputComponent);
