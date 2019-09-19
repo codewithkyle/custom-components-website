6607312f08a4836ca6b6aedc1620f226cc6a0d3f
Kyle Andrews <codingwithkyle@gmail.com>
2019-09-19 12:24:42 +0000

    Updates

--- assets/1568819233322/text-inputs/outline.js
+++
@@ -1,6 +0,0 @@
-class OutlineComponent extends HTMLElement {
-    connectedCallback() {
-        console.log('outline component has been connected.');
-    }
-}
-customElements.define('outline-component', OutlineComponent);
