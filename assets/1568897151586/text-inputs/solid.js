230bb9270b5839361f4947cd2698c652631ff706
Kyle Andrews <codingwithkyle@gmail.com>
2019-09-19 12:45:52 +0000

    Updates

---
+++ assets/1568897151586/text-inputs/solid.js
@@ -0,0 +1,26 @@
+class SolidInputComponent extends HTMLElement {
+    constructor() {
+        super();
+        this.handleBlurEvent = this.validate.bind(this);
+        this._input = this.querySelector('input');
+    }
+    validate() {
+        if (this._input.value === '') {
+            this._input.classList.remove('has-value');
+        }
+        else {
+            this._input.classList.add('has-value');
+        }
+        if (!this._input.validity.valid) {
+            this._input.classList.add('is-invalid');
+            this._input.reportValidity();
+        }
+        else {
+            this._input.classList.remove('is-invalid');
+        }
+    }
+    connectedCallback() {
+        this._input.addEventListener('blur', this.handleBlurEvent);
+    }
+}
+customElements.define('solid-input-component', SolidInputComponent);
