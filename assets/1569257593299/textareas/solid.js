54b1b383ce537f67b933ec6e90d88de3483538e0
Kyle Andrews <codingwithkyle@gmail.com>
2019-09-23 16:53:14 +0000

    Updates

---
+++ assets/1569257593299/textareas/solid.js
@@ -0,0 +1,26 @@
+class SolidTextareaComponents extends HTMLElement {
+    constructor() {
+        super();
+        this.handleBlurEvent = this.validate.bind(this);
+        this._textarea = this.querySelector('textarea');
+    }
+    validate() {
+        if (this._textarea.value === '') {
+            this._textarea.classList.remove('has-value');
+        }
+        else {
+            this._textarea.classList.add('has-value');
+        }
+        if (!this._textarea.validity.valid && !this._textarea.classList.contains('is-invalid')) {
+            this._textarea.classList.add('is-invalid');
+            this._textarea.reportValidity();
+        }
+        else {
+            this._textarea.classList.remove('is-invalid');
+        }
+    }
+    connectedCallback() {
+        this._textarea.addEventListener('blur', this.handleBlurEvent);
+    }
+}
+customElements.define('solid-textarea-component', SolidTextareaComponents);
