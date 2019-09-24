937940f9d3c8a4d1b67c12915877c951eb9faaf2
Kyle Andrews <codingwithkyle@gmail.com>
2019-09-24 17:50:12 +0000

    Updates

--- worker.js
+++ worker.js
@@ -1,32 +1,26 @@
-// let currentTimestamp = 'initial';
-// self.addEventListener('message', (event)=>{
-//     if (event.data.application)
-//     {
-//         currentTimestamp = event.data.application;
-//         caches.keys().then((cacheNames)=>{
-//             return Promise.all(
-//                 cacheNames.map((cacheName)=>{
-//                     if (cacheName !== currentTimestamp)
-//                     {
-//                         return caches.delete(cacheName);
-//                     }
-//                 })
-//             );
-//         });
-//     }
-// });
-// self.addEventListener('fetch', (event:any) => {
-//     event.respondWith(
-//         caches.match(event.request).then((resp) => {
-//             return resp || fetch(event.request).then((response) => {
-//                 let responseClone = response.clone();
-//                 caches.open(currentTimestamp).then((cache) => {
-//                     cache.put(event.request, responseClone);
-//                 });
-//                 return response;
-//             });
-//         }).catch(() => {
-//             return new Response('No internet');
-//         })
-//     );
-// });
+let currentTimestamp = 'initial';
+self.addEventListener('message', (event) => {
+    if (event.data.application) {
+        currentTimestamp = event.data.application;
+        caches.keys().then((cacheNames) => {
+            return Promise.all(cacheNames.map((cacheName) => {
+                if (cacheName !== currentTimestamp) {
+                    return caches.delete(cacheName);
+                }
+            }));
+        });
+    }
+});
+self.addEventListener('fetch', (event) => {
+    event.respondWith(caches.match(event.request).then((resp) => {
+        return resp || fetch(event.request).then((response) => {
+            let responseClone = response.clone();
+            caches.open(currentTimestamp).then((cache) => {
+                cache.put(event.request, responseClone);
+            });
+            return response;
+        });
+    }).catch(() => {
+        return new Response('No internet');
+    }));
+});
