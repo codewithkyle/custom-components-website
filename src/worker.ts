c37d8d4b80e4f1b2aa76209f7b722bea6ddc4d3c
Kyle Andrews <kylea@page.works>
2019-09-24 13:49:28 -0400

    Enabling the service worker

--- src/worker.ts
+++ src/worker.ts
@@ -1,36 +1,36 @@
-// let currentTimestamp = 'initial';
+let currentTimestamp = 'initial';
 
-// self.addEventListener('message', (event)=>{
-//     if (event.data.application)
-//     {
-//         currentTimestamp = event.data.application;
+self.addEventListener('message', (event)=>{
+    if (event.data.application)
+    {
+        currentTimestamp = event.data.application;
 
-//         caches.keys().then((cacheNames)=>{
-//             return Promise.all(
-//                 cacheNames.map((cacheName)=>{
-//                     if (cacheName !== currentTimestamp)
-//                     {
-//                         return caches.delete(cacheName);
-//                     }
-//                 })
-//             );
-//         });
-//     }
-// });
+        caches.keys().then((cacheNames)=>{
+            return Promise.all(
+                cacheNames.map((cacheName)=>{
+                    if (cacheName !== currentTimestamp)
+                    {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        });
+    }
+});
 
-// self.addEventListener('fetch', (event:any) => {
-//     event.respondWith(
-//         caches.match(event.request).then((resp) => {
-//             return resp || fetch(event.request).then((response) => {
-//                 let responseClone = response.clone();
-//                 caches.open(currentTimestamp).then((cache) => {
-//                     cache.put(event.request, responseClone);
-//                 });
+self.addEventListener('fetch', (event:any) => {
+    event.respondWith(
+        caches.match(event.request).then((resp) => {
+            return resp || fetch(event.request).then((response) => {
+                let responseClone = response.clone();
+                caches.open(currentTimestamp).then((cache) => {
+                    cache.put(event.request, responseClone);
+                });
 
-//                 return response;
-//             });
-//         }).catch(() => {
-//             return new Response('No internet');
-//         })
-//     );
-// });
+                return response;
+            });
+        }).catch(() => {
+            return new Response('No internet');
+        })
+    );
+});
