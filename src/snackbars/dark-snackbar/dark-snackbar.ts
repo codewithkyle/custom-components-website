bd057f880d68fba9cbe8081abfc3992739c34462
Kyle Andrews <kylea@page.works>
2019-09-27 19:43:46 -0400

    Cleans up snackbar demo

--- src/snackbars/dark-snackbar/dark-snackbar.ts
+++
@@ -1,9 +0,0 @@
-class DarkSnackbar extends HTMLElement
-{
-	connectedCallback()
-	{
-		console.log('dark-snackbar component has been connected.');
-	}
-}
-
-customElements.define('dark-snackbar', DarkSnackbar);
